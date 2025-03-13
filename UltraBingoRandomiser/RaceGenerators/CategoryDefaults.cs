8ecc894de14f29ed693987d3e1a5a45646befc93
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-03-13 10:15:39 +0000

    Move CategoryDefaults to its own file

---
+++ UltraBingoRandomiser/RaceGenerators/CategoryDefaults.cs
@@ -0,0 +1,19 @@
+﻿namespace UltrakillRaceGenerator.RaceGenerators;
+
+[Flags]
+internal enum CategoryDefaults
+{
+    None = 0,
+    Any = 1,
+    P = 2,
+    Nomo = 4,
+    Nomow = 8,
+    InboundsAny = 16,
+    InboundsP = 32,
+    InboundsNomo = 64,
+    Inbounds = InboundsAny | InboundsP,
+    Weaponless = 128,
+    Check = 256,
+    Special = 512,
+    All = Any | P | Nomo | Nomow | InboundsAny | InboundsP | InboundsNomo | Weaponless | Check | Special
+}
