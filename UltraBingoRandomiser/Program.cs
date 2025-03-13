f79eafb50e06b53f89bda1ec0be55abe59a1d180
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-03-13 10:17:31 +0000

    Add option to generate a Standard Race

--- UltraBingoRandomiser/Program.cs
+++ UltraBingoRandomiser/Program.cs
@@ -8,6 +8,7 @@ internal class Program
     private static void Main(string[] args) // TODO: Add command line arguments to automatically select an option in the menu
     {
         MainMenu.Instance.optionFuncs.Add("Generate Bingo Board", PrintNewBingoBoard);
+        MainMenu.Instance.optionFuncs.Add("Generate Standard Race Options", PrintNewStandardRace);
         MainMenu.Instance.LoadMenu();
     }
 
@@ -20,5 +21,14 @@ internal class Program
         string bingo = UltraBingo.GenerateBingoBoard(easy, medium, hard);
         Console.WriteLine(bingo);
         ClipboardUtility.SetText(bingo);
+    }
+
+    private static void PrintNewStandardRace()
+    {
+        string[] strings = StandardRaces.GetRandomStandardRaces();
+        foreach (string s in strings)
+        {
+            Console.WriteLine(s);
+        }
     }
 }
